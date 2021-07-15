d670af678db6fef6f12467c37193bbf1bd18833a
dungduy02 <18130048@st.hcmuaf.edu.vn>
2021-07-15 22:43:39 +0700

    delete

--- app/src/main/java/com/dungduy/demo/appplaymusic/Service/APIService.java
+++
@@ -1,9 +0,0 @@
-package com.dungduy.demo.appplaymusic.Service;
-
-public class APIService {
-    private static String base_url = "https://appmusicplay.000webhostapp.com/Server/";
-
-    public static Dataservice getService(){
-        return APIRetrofitClient.getClient(base_url).create(Dataservice.class);
-    }
-}
